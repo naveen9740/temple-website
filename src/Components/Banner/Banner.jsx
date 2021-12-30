a3d316e043b43cb8a84394974e2c74c32dd31547
NAVEEN KAMATH <naveen.kamath002@gmail.com>
2021-12-30 06:43:31 +0530

    refactor: image loading

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -1,12 +1,11 @@
 import "./Banner.css";
 import { Images } from "../../data";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 export const Banner = () => {
   let [img, setImg] = useState(0);
   setTimeout(() => {
     setImg(img >= 4 ? 0 : img + 1);
   }, 6000);
-  console.log(img);
   return (
     <div className="banner">
       <img
@@ -17,6 +16,26 @@ export const Banner = () => {
           backgroundRepeat: "no-repeat",
         }}
         alt=""
+      />
+      <img
+        style={{ display: "none" }}
+        src="https://i.ibb.co/9whQZkS/templwm.jpg"
+        alt="templ1"
+      />
+      <img
+        style={{ display: "none" }}
+        src="https://i.ibb.co/cJh6dPJ/templ6.jpg"
+        alt="templ2"
+      />
+      <img
+        style={{ display: "none" }}
+        src="https://i.ibb.co/jZVMSnF/templ3.jpg"
+        alt="templ3"
+      />
+      <img
+        style={{ display: "none" }}
+        src="https://i.ibb.co/9wWCYvm/temp2.jpg"
+        alt="templ4"
       />
     </div>
   );
